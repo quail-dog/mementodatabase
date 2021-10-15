f7154f26df412805d27cee47e02364ea1cd259d0
quail-dog <92603055+quail-dog@users.noreply.github.com>
2021-10-15 14:39:19 -0500

    Create test.js
    Initial commit

---
+++ test.js
@@ -0,0 +1,12 @@
+/**
+Issue a search query to Discogs database.
+@param {string} query - Search query.
+
+function Check (query) {
+    if( query.includes('INTERAC')) {
+    var type = "Interact"
+    }
+    else {
+    var type = "Null"
+    }
+}
